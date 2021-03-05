6f666131368b7ff0a3fc31615072023128fa9c13
Brian Forbes <brianandrewf81@gmail.com>
2021-03-04 22:13:53 -0700

    Import Link and wrap Get Random button with a Link.

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import './HomePage.css';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
@@ -11,9 +12,11 @@ const HomePage = () => {
         <p className="app-description">Welcome to Let's Taco 'Bout it, your somewhat useful taco recipe randomizer. On the next page you will have the choice to generate a taco at random, either a recipe that has been curated
         by the contributors with a well vetted flavor combination, or a WACKY taco where all of the ingredients are combined at random! Click the button below to get started on your road to flavor town!'
         </p>
-        <button className="get-random">
-        GET RANDOM!!!
-        </button>
+        <Link to={'/'}>
+          <button className="get-random">
+            GET RANDOM!!!
+          </button>
+        </Link>
       </main>
     </div>
   )
