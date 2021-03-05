4655785c48613403133f0d63eb8fe4cf3dab0238
Brian Forbes <brianandrewf81@gmail.com>
2021-03-04 23:04:45 -0700

    Fetch and display taco name in from API in RandomTaco component

--- src/RandomTaco.js
+++ src/RandomTaco.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './HomePage'
 import { Link } from 'react-router-dom';
+import { getRandomCuratedTaco, getRandomWackyTaco } from './fetchRequests';
 
 class RandomTaco extends React.Component {
   constructor() {
@@ -8,9 +9,27 @@ class RandomTaco extends React.Component {
     this.state = {
       currentTaco: {},
       savedTacos: [],
+      errorMessage: ""
     };
   }
 
-  
+  componentDidMount = () => {
+  fetch('http://taco-randomizer.herokuapp.com/random/?full-taco=true')
+  .then(response => response.json())
+  .then(data => this.setState({
+    currentTaco: data,
+  }))
+.catch(error => {
+      this.setState({errorMessage: error})
+    })
+}
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.currentTaco.name}</h1>
+      </div>
+    )
+  }
 }
 export default RandomTaco;
