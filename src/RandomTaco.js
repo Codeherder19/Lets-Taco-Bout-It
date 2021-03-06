31b762996c9c0880ba440ddd16eb04db612f85bd
Brian Forbes <brianandrewf81@gmail.com>
2021-03-05 21:25:43 -0700

    Set up ternary operator to conditionally set state for curated taco

--- src/RandomTaco.js
+++ src/RandomTaco.js
@@ -9,23 +9,40 @@ class RandomTaco extends React.Component {
     super()
     this.state = {
       currentTaco: {},
-      currentBaseLayerName: "",
-      currentBaseLayerRecipe: "",
-      currentShellName: "",
-      currentShellRecipe: "",
-      currentMixinName: "",
-      currentMixinRecipe: "",
-      currentCondimentName: "",
-      currentCondimentRecipe: "",
-      currentSeasoningName: "",
-      currentSeasoningRecipe: "",
+      currentBaseLayerName: null,
+      currentBaseLayerRecipe: null,
+      currentShellName: null,
+      currentShellRecipe: null,
+      currentMixinName: null,
+      currentMixinRecipe: null,
+      currentCondimentName: null,
+      currentCondimentRecipe: null,
+      currentSeasoningName: null,
+      currentSeasoningRecipe: null,
       savedTacos: [],
-      errorMessage: ""
+      errorMessage: null
     };
   }
 
   capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  fetchCuratedTaco = () => {fetch('http://taco-randomizer.herokuapp.com/random/?full-taco=true')
+  .then(response => response.json())
+  .then(data => this.setState({
+    currentTaco: data,
+    currentBaseLayerName: data.base_layer.name ? this.capitalizeFirstLetter(data.base_layer.name) : null,
+    currentBaseLayerRecipe: data.base_layer.recipe ? data.base_layer.recipe.replace(/[^.,\sa-zA-Z]/g, '') : null,
+    currentShellName: data.shell ? this.capitalizeFirstLetter(data.shell.name) : null,
+    currentShellRecipe: data.shell ? data.shell.recipe.replace(/[^.,\sa-zA-Z]/g, '') : null,
+    currentMixinName: data.mixin ? data.mixin.name : null,
+    currentMixinRecipe: data.mixin ? data.mixin.recipe.replace(/[^.,\sa-zA-Z]/g, '') : null,
+    currentCondimentName: data.condiment ? data.condiment.name : null,
+    currentCondimentRecipe: data.condiment ? data.condiment.recipe.replace(/[^.,\sa-zA-Z]/g, '') : null,
+    currentSeasoningName: data.seasoning ? data.seasoning.name : null,
+    currentSeasoningRecipe: data.seasoning ? data.seasoning.recipe.replace(/[^.,\sa-zA-Z]/g, '') : null
+  }))
   }
 
   fetchWackyTaco = () => {fetch('http://taco-randomizer.herokuapp.com/random/')
@@ -53,36 +70,40 @@ class RandomTaco extends React.Component {
 
 
   render() {
-
     return (
       <main className="random-taco-main">
         <div className="recipe-container">
+          {this.state.currentBaseLayerName &&
           <div className="ingredient-container">
             <h2 className="ingredient-name">{this.state.currentBaseLayerName}:</h2>
             <p className="ingredient-recipe">{this.state.currentBaseLayerRecipe.replace(`${this.state.currentBaseLayerName.replace(/[^.,\sa-zA-Z]/g, '')}`, "")}`, "")}</p>
-          </div>
+          </div>}
+          {this.state.currentShellName &&
           <div className="ingredient-container">
             <h2 className="ingredient-name">{this.state.currentShellName}:</h2>
             <p className="ingredient-recipe">{this.state.currentShellRecipe.replace(`${this.state.currentShellName.replace(/[^.,\sa-zA-Z]/g, '')}`, "")}</p>
-          </div>
+          </div>}
+          {this.state.currentMixinName &&
           <div className="ingredient-container">
             <h2 className="ingredient-name">{this.state.currentMixinName}:</h2>
             <p className="ingredient-recipe">{this.state.currentMixinRecipe.replace(`${this.state.currentMixinName.replace(/[^.,\sa-zA-Z]/g, '')}`, "")}</p>
-          </div>
+          </div>}
+          {this.state.currentCondimentName &&
           <div className="ingredient-container">
             <h2 className="ingredient-name">{this.state.currentCondimentName}:</h2>
             <p className="ingredient-recipe">{this.state.currentCondimentRecipe.replace(`${this.state.currentCondimentName.replace(/[^.,\sa-zA-Z]/g, '')}`, "")}</p>
-          </div>
+          </div>}
+          {this.state.currentSeasoningName &&
           <div className="ingredient-container">
             <h2 className="ingredient-name">{this.state.currentSeasoningName}:</h2>
             <p className="ingredient-recipe">{this.state.currentSeasoningRecipe.replace(`${this.state.currentSeasoningName.replace(/[^.,\sa-zA-Z]/g, '')}`, "")}</p>
-          </div>
+          </div>}
         </div>
         <div className="button-container">
           <Link to="/">
             <button className="back-to-home">Back to Home</button>
           </Link>
-          <button className="random-taco">Curated Taco</button>
+          <button className="random-taco" onClick={this.fetchCuratedTaco}>Curated Taco</button>
           <button className="random-taco" onClick={this.fetchWackyTaco}>WACKY TACO!!!</button>
         </div>
       </main>
