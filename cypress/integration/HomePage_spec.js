266478e7d4bda9507ae4f1c1181b0fbeffcac1a3
Brian Forbes <brianandrewf81@gmail.com>
2021-03-07 02:57:41 -0700

    Test image on home page component

---
+++ cypress/integration/HomePage_spec.js
@@ -0,0 +1,15 @@
+describe('Header', () => {
+  const baseUrl = 'http://localhost:3000';
+
+  beforeEach(() => {
+    cy
+    .visit(baseUrl);
+  });
+
+  it('should have an image of some tacos on the home page', () => {
+    cy
+    .get('img').should('have.attr', 'src', 'https://media.istockphoto.com/photos/spicy-homemade-beef-barbacoa-tacos-picture-id960337396?k=6&m=960337396&s=612x612&w=0&h=JHvHpv-7OmGIklZd1qWPsQ_OfwwKiheorszp143RVg8=')
+  })
+
+
+});
