dd6061f6cebd6c067bc6532fae9257a8bdaef681
Brian Forbes <brianandrewf81@gmail.com>
2021-03-07 00:38:19 -0700

    Add button to navigate back to random taco generator

--- src/SavedTacos.js
+++ src/SavedTacos.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './SavedTacos.css'
+import { Link } from 'react-router-dom';
 
 const SavedTacos = (props) => {
   console.log(props)
   return (
-    <h1>Saved Tacos</h1>
+    <main className="saved-tacos">
+      <h1>Saved Tacos</h1>
+      <Link to="/RandomTaco">
+        <button className="back-to-random">Back to Random Taco Generator</button>
+      </Link>
+    </main>
   )
 }
 
