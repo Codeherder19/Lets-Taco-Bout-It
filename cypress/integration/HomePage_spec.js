52ed0f7d7718e5700911c9e9e52f53a2bcb6a2c6
Brian Forbes <brianandrewf81@gmail.com>
2021-03-07 03:01:10 -0700

    Test paragraph that contains the description of the application

--- cypress/integration/HomePage_spec.js
+++ cypress/integration/HomePage_spec.js
@@ -11,5 +11,10 @@ describe('Header', () => {
     .get('img').should('have.attr', 'src', 'https://media.istockphoto.com/photos/spicy-homemade-beef-barbacoa-tacos-picture-id960337396?k=6&m=960337396&s=612x612&w=0&h=JHvHpv-7OmGIklZd1qWPsQ_OfwwKiheorszp143RVg8=')
   })
 
+  it('should have an paragraph that describes the app', () => {
+    cy
+    .get('p[class=app-description]').contains(`Welcome to Let's Taco 'Bout it`)
+  })
+
 
 });
