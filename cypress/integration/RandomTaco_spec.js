9ca647e50ef566380890362ebfa8b56c79966652
Brian Forbes <brianandrewf81@gmail.com>
2021-03-07 14:37:45 -0700

    Tests back button will navigate user from RandomTaco to HomePage

--- cypress/integration/RandomTaco_spec.js
+++ cypress/integration/RandomTaco_spec.js
@@ -44,4 +44,23 @@ describe('Random Taco', () => {
     })
   })
 
+  it('should have a button that will take you back to the home page', () => {
+    cy
+    .get('.back-to-home').click()
+    cy
+    .get('img').should('have.attr', 'src', 'https://media.istockphoto.com/photos/spicy-homemade-beef-barbacoa-tacos-picture-id960337396?k=6&m=960337396&s=612x612&w=0&h=JHvHpv-7OmGIklZd1qWPsQ_OfwwKiheorszp143RVg8=')
+  })
+
+  // it('should have buttons that generate either a random curated taco or a wacky taco', () => {
+  //
+  // })
+  //
+  // it('should have a button to save a taco to be viewed later', () => {
+  //
+  // })
+  //
+  // it('should have a button that takes a user to view saved tacos', () => {
+  //
+  // })
+
 });
