5bfca5dea7f558a9d84ffb5b8ba1fc1dc72e2db1
Brian Forbes <brianandrewf81@gmail.com>
2021-03-08 17:23:22 -0700

    Refactor SavedTacos component to render Taco component with delete button

--- src/SavedTacos.js
+++ src/SavedTacos.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import './SavedTacos.css'
 import { Link } from 'react-router-dom';
-import TinyTaco from './TinyTaco'
+import Taco from './Taco'
 
-const SavedTacos = (props) => {
-  console.log(props.savedTacos)
-  const allTacos = props.savedTacos.map((taco, index) => <TinyTaco
-    key={index + 1}
-    id={index + 1}
-    name={taco.name || `Taco #${taco.id}`}
-   />)
+const SavedTacos = ({savedTacos, deleteSaved}) => {
+
+  const allTacos = () => {
+    if (savedTacos.length) {
+      return savedTacos.map(taco => {
+        console.log(taco)
+        return (
+        <div>
+        <Taco
+          id={taco.id}
+          key={taco.name}
+          name={taco.name || `Taco #${taco.id}`}
+          baseLayerName={taco.base_layer.name}
+          baseLayerRecipe={taco.base_layer.recipe}
+          shellName={taco.shell ? taco.shell.name : `Dealer's choice`}
+          shellRecipe={taco.shell ? taco.shell.recipe : `Flour or Corn tortilla`}
+          mixinName={taco.mixin ? taco.mixin.name : ""}
+          mixinRecipe={taco.mixin ? taco.mixin.recipe : ""}
+          condimentName={taco.condiment ? taco.condiment.name : ""}
+          condimentRecipe={taco.condiment ? taco.condiment.recipe : ""}
+          seasoningName={taco.seasoning ? taco.seasoning.name : ""}
+          seasoningRecipe={taco.seasoning ? taco.seasoning.recipe : ""}
+        />
+        <button className="delete-taco" id={taco.id} onClick={ (event) => {deleteSaved(event.target.id)}}>Remove</button>
+        </div>
+      )
+      })
+    }
+    }
+
   return (
     <main className="saved-tacos">
       <h2 className="section-title">Saved Tacos</h2>
-      <section className="tiny-taco-container">{allTacos}</section>
+      <section className="saved-tacos-container">{allTacos()}</section>
       <Link to="/RandomTaco">
         <button className="back-to-random">Back to Random Taco Generator</button>
       </Link>
